c783969664bd4b7b0b8c31e2957597df40523480
Margo <m.yunanova@gmail.com>
2025-01-19 15:19:23 +0300

    feat: implement main app and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,57 @@
-import './App.css';
+import { useState } from 'react';
+import { Link, Outlet } from 'react-router-dom';
+
+import styles from './App.module.css';
+
+const tabNames = [
+  { name: 'Все котики', url: '/' },
+  { name: 'Любимые котики', url: '/favorites' },
+];
 
 function App() {
-  return <></>;
+  const [activeTab, setActiveTab] = useState(0);
+
+  const handleTabClick = (tabIndex: number) => {
+    setActiveTab(tabIndex);
+  };
+
+  return (
+    <>
+      <header className={styles.header}>
+        <div className={styles.tabs}>
+          <nav className="tab-nav">
+            <ul
+              className={styles['tab-list']}
+              role="tablist"
+              aria-orientation="horizontal"
+            >
+              {tabNames.map((tabName, i) => (
+                <li className={styles['tab-item']} key={i}>
+                  <Link to={tabName.url}>
+                    <button
+                      onClick={() => handleTabClick(i)}
+                      role="tab"
+                      id={`tab-${i}`}
+                      aria-controls={`panel-${i}`}
+                      aria-selected="false"
+                      className={`${styles['tab-btn']} ${
+                        activeTab === i && styles['tab-btn_active']
+                      }`}
+                    >
+                      {tabName.name}
+                    </button>
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </nav>
+        </div>
+      </header>
+      <main className={styles.main}>
+        <Outlet />
+      </main>
+    </>
+  );
 }
 
 export default App;
